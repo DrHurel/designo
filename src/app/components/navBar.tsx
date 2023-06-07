bf9134287b0099a38471c8f88ad3b1bbbe4835d6
DrHurel <jeremyhurel34@gmail.com>
2023-06-07 23:19:10 +0200

    detail to do

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -1,14 +1,42 @@
+"use client"
+
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
 export function NavBar() {
 
+  const [menu, setMenu] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        document.querySelector('nav')?.classList.add('scroll')
+      } else {
+        document.querySelector('nav')?.classList.remove('scroll')
+      }
+    }
+    )
+
+
+  }, [])
+
   return <nav>
     <Link href="/"><img src="/assets/shared/desktop/logo-dark.png" alt="logo" /></Link>
-    <ul>
-      <Link href="/about">Our Company</Link>
-      <Link href="/locations">Locations</Link>
-      <Link href="/contact">Contact</Link>
-    </ul>
+    <div>
+      <input type="checkbox" name="menu" id="menu" />
+      <div className="anim">
+
+
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul>
+        <Link href="/about">Our Company</Link>
+        <Link href="/locations">Locations</Link>
+        <Link href="/contact">Contact</Link>
+      </ul>
+    </div>
   </nav>
 
 }
