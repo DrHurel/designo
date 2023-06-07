2e9e14dff7eebf5b889065b9becb076a40732e96
DrHurel <jeremyhurel34@gmail.com>
2023-06-07 14:20:13 +0200

    Intragration done todo : details & form control

--- src/app/components/callToAction.tsx
+++ src/app/components/callToAction.tsx
@@ -5,9 +5,12 @@ export function CallToAction() {
   return <section className="callToAction">
 
 
-    <div> <h2>Let’s talk about your project</h2>
-      <p>Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.
-      </p></div>
+    <div>
+      <h2>Let’s talk about your project</h2>
+      <p>
+        Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.
+      </p>
+    </div>
 
     <Link href="/contact"><button>Get in touch</button></Link>
 
