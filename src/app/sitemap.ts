9338b48885d1167d1f5eefd8a9b386cde9668a4f
DrHurel <jeremyhurel34@gmail.com>
2023-06-17 08:14:19 +0200

    add basic SEO

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://designo-two.vercel.app/contact',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://designo-two.vercel.app/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://designo-two.vercel.app/app-design',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://designo-two.vercel.app/web-design',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://designo-two.vercel.app/graphic-design',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://designo-two.vercel.app/locations',
+      lastModified: new Date(),
+    },
+  ]
+}
